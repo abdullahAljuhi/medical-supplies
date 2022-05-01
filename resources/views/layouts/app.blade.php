088c2300b23a7f8a2078415dbd17d1fb4c372ee0
Murad77Hassan <murad7727hassan@gmail.com>
2022-05-01 23:00:51 +0300

    create basic design for ads page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,6 +29,7 @@
     <link href="{{ asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/viewAndList.css') }}">
     {{-- fotn awesom  --}}
      
     <link rel="stylesheet" href="{{ asset('css/fontawesome.min.css') }}">
@@ -387,6 +388,8 @@
 <script src="{{ asset('assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
 <script src="{{ asset('assets/vendor/tinymce/tinymce.min.js') }}"></script>
 <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
+<script src="{{asset('js/viewAndList.js ') }}"></script>
+
 
 <!-- Template Main JS File -->
 <script src="{{ asset('assets/js/main.js') }}"></script>
--- routes/web.php
+++ routes/web.php
@@ -85,7 +85,7 @@ Route::group(['middleware' => 'auth'], function () {
 
 
 Route::get('/', function () {
-    return view('pharmcy-profile');
+    return view('ads');
 });
 Route::get('/about', function () {
     return view('about');
