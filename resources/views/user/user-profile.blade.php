e64cc1179271e5f3a3cccec006ef4d02e6409af1
Baaboud <Baaboud.dev@gmail.com>
2022-05-15 15:29:49 +0300

    change layouts

--- resources/views/user/user-profile.blade.php
+++ resources/views/user/user-profile.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <!-- Page Title -->
     <div class="pagetitle">
-        <h1>Profile</h1>
+        <h1>اعدادات الحساب</h1>
         <nav>
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="../index.blade.php">Home</a></li>
