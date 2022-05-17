4559f17ed1a1770aa21080581f16af93186e69ec
abdullahAljuhi <95518730+abdullahAljuhi@users.noreply.github.com>
2022-05-17 16:58:35 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,23 +21,23 @@
         rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/viewAndList.css') }}">
+    <link href="{{ secure_asse('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asse('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ secure_asse('assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asse('assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ secure_asse('assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+    <link href="{{ secure_asse('assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ secure_asse('assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ secure_asse('css/index.css') }}">
+    <link rel="stylesheet" href="{{ secure_asse('css/viewAndList.css') }}">
     {{-- fotn awesom --}}
 
-    <link rel="stylesheet" href="{{ asset('css/fontawesome.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asse('css/fontawesome.min.css') }}">
     <!-- Template Main CSS File -->
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+    <link href="{{ secure_asse('assets/css/style.css') }}" rel="stylesheet">
     <!-- Costume CSS Files -->
-    <link rel="stylesheet" href="{{ asset('assets/css/ar.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+    <link rel="stylesheet" href="{{ secure_asse('assets/css/ar.css') }}">
+    <link rel="stylesheet" href="{{ secure_asse('css/index.css') }}">
     @yield('extra-style')
 
 </head>
